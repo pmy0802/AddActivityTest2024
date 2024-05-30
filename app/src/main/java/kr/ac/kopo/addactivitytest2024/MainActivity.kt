7a7252bc79b06fccd23997c299fa9a6c25345a9f
pmy0802 <pmy0802@naver.com>
2024-05-30 16:26:24 +0900

    직접풀어보기 10-1 완성

--- app/src/main/java/kr/ac/kopo/addactivitytest2024/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/addactivitytest2024/MainActivity.kt
@@ -4,15 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
+    lateinit var rg : RadioGroup
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        var rg = findViewById<RadioGroup>(R.id.rg)
+        rg.check(R.id.radioSecond)
         var btnMain = findViewById<Button>(R.id.btnMain)
+        var intent = Intent(this@MainActivity, SecondActivity::class.java)
+        rg.setOnCheckedChangeListener{ group, chekedId ->
+            when(chekedId){
+                R.id.radioSecond -> intent = Intent(this@MainActivity, SecondActivity::class.java)
+                R.id.radioThird -> intent = Intent(this@MainActivity, ThirdActivity::class.java)
+            }
+
+        }
         btnMain.setOnClickListener{
-            var intent = Intent(applicationContext, SecondActivity::class.java)
             startActivity(intent)
         }
     }
